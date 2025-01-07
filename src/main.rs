7b0d8e905af9efbee9c88d6a5bcfc178db1cffd2
Robert O'Shea <robertosheagamedev@gmail.com>
2025-01-07 22:37:45 +0000

    Switch to 2D

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,16 @@
-use std::f32::consts::FRAC_PI_2;
-
 use bevy::{
     color::palettes::tailwind,
     core::FrameCount,
-    input::mouse::AccumulatedMouseMotion,
-    pbr::NotShadowCaster,
     prelude::*,
     render::{
         settings::{Backends, RenderCreation},
-        view::RenderLayers,
         RenderPlugin,
     },
+    window::WindowResolution,
 };
-
-const DEFAULT_RENDER_LAYER: usize = 0;
-const VIEW_MODEL_RENDER_LAYER: usize = 1;
 
 #[derive(Debug, Component)]
 struct Player;
-
-#[derive(Debug, Component, Deref, DerefMut)]
-struct CameraSensitivity(Vec2);
-
-impl Default for CameraSensitivity {
-    fn default() -> Self {
-        Self(Vec2::new(0.003, 0.002))
-    }
-}
-
-#[derive(Debug, Component)]
-struct WorldModelCamera;
 
 fn main() {
     App::new()
@@ -37,8 +18,10 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Bevy Craft".to_string(),
+                        title: "Rogue".to_string(),
                         visible: false,
+                        resolution: WindowResolution::new(800.0, 600.0)
+                            .with_scale_factor_override(3.0),
                         ..default()
                     }),
                     ..default()
@@ -51,13 +34,11 @@ fn main() {
                         },
                     ),
                     ..default()
-                }),
+                })
+                .set(ImagePlugin::default_nearest()),
         )
-        .add_systems(
-            Startup,
-            (spawn_view_models, spawn_world_models, spawn_lights),
-        )
-        .add_systems(Update, (make_visible, move_player))
+        .add_systems(Startup, (spawn_player))
+        .add_systems(Update, (make_visible))
         .run();
 }
 
@@ -67,111 +48,59 @@ fn make_visible(mut window: Single<&mut Window>, frames: Res<FrameCount>) {
     }
 }
 
-fn spawn_view_models(
+fn spawn_player(
     mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
 ) {
-    let arm = meshes.add(Cuboid::new(0.1, 0.1, 0.5));
-    let arm_matieral = materials.add(Color::from(tailwind::TEAL_200));
-
-    commands
-        .spawn((
-            Player,
-            CameraSensitivity::default(),
-            Transform::from_xyz(0.0, 1.0, 0.0),
-            Visibility::default(),
-        ))
-        .with_children(|parent| {
-            parent.spawn((
-                WorldModelCamera,
-                Camera3d::default(),
-                Projection::from(PerspectiveProjection {
-                    fov: 90.0_f32.to_radians(),
-                    ..default()
-                }),
+    let handle = asset_server.load("sprites/minirogue.png");
+    let layout = TextureAtlasLayout::from_grid(UVec2::splat(8), 16, 13, None, None);
+    let texture_atlas_layout = texture_atlas_layouts.add(layout);
+    commands.spawn(Camera2d);
+    for y in 0..10 {
+        if y == 0 || y == 9 {
+            let y = y as f32;
+            for x in 0..10 {
+                let x = x as f32;
+                commands.spawn((
+                    Sprite::from_atlas_image(
+                        handle.clone(),
+                        TextureAtlas {
+                            layout: texture_atlas_layout.clone(),
+                            index: 1,
+                        },
+                    ),
+                    Player,
+                    Transform::from_xyz(x * 8.0, 1.0 + y * 8.0, 0.0),
+                    Visibility::default(),
+                ));
+            }
+        } else {
+            let y = y as f32;
+            commands.spawn((
+                Sprite::from_atlas_image(
+                    handle.clone(),
+                    TextureAtlas {
+                        layout: texture_atlas_layout.clone(),
+                        index: 1,
+                    },
+                ),
+                Player,
+                Transform::from_xyz(0.0 + 0.0 * 8.0, 1.0 + y * 8.0, 0.0),
+                Visibility::default(),
             ));
-
-            parent.spawn((
-                Camera3d::default(),
-                Camera {
-                    order: 1,
-                    ..default()
-                },
-                Projection::from(PerspectiveProjection {
-                    fov: 70.0_f32.to_radians(),
-                    ..default()
-                }),
-                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
+            commands.spawn((
+                Sprite::from_atlas_image(
+                    handle.clone(),
+                    TextureAtlas {
+                        layout: texture_atlas_layout.clone(),
+                        index: 1,
+                    },
+                ),
+                Player,
+                Transform::from_xyz(0.0 + 9.0 * 8.0, 1.0 + y * 8.0, 0.0),
+                Visibility::default(),
             ));
-
-            parent.spawn((
-                Mesh3d(arm),
-                MeshMaterial3d(arm_matieral),
-                Transform::from_xyz(0.2, -0.1, -0.25),
-                RenderLayers::layer(VIEW_MODEL_RENDER_LAYER),
-                NotShadowCaster,
-            ));
-        });
-}
-
-fn spawn_world_models(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    let floor = meshes.add(Plane3d::new(Vec3::Y, Vec2::splat(10.0)));
-    let cube = meshes.add(Cuboid::new(2.0, 0.5, 1.0));
-    let material = materials.add(Color::WHITE);
-
-    commands.spawn((Mesh3d(floor), MeshMaterial3d(material.clone())));
-
-    commands.spawn((
-        Mesh3d(cube.clone()),
-        MeshMaterial3d(material.clone()),
-        Transform::from_xyz(0.0, 0.25, -3.0),
-    ));
-
-    commands.spawn((
-        Mesh3d(cube),
-        MeshMaterial3d(material),
-        Transform::from_xyz(0.75, 1.75, 0.0),
-    ));
-}
-
-fn spawn_lights(mut commands: Commands) {
-    commands.spawn((
-        PointLight {
-            color: Color::from(tailwind::ROSE_300),
-            shadows_enabled: true,
-            ..default()
-        },
-        Transform::from_xyz(-2.0, 4.0, -0.75),
-        // The light source illuminates both the world model and the view model.
-        RenderLayers::from_layers(&[DEFAULT_RENDER_LAYER, VIEW_MODEL_RENDER_LAYER]),
-    ));
-}
-
-fn move_player(
-    accumulated_mouse_motion: Res<AccumulatedMouseMotion>,
-    mut player: Query<(&mut Transform, &CameraSensitivity), With<Player>>,
-) {
-    let Ok((mut transform, camera_sensitivity)) = player.get_single_mut() else {
-        return;
-    };
-    let delta = accumulated_mouse_motion.delta;
-
-    // no need to multiply by deltatime because accumulated_mouse_motion is measured at a per frame level
-    if delta != Vec2::ZERO {
-        let delta_yaw = -delta.x * camera_sensitivity.x;
-        let delta_pitch = -delta.y * camera_sensitivity.y;
-
-        let (yaw, pitch, roll) = transform.rotation.to_euler(EulerRot::YXZ);
-        let yaw = yaw + delta_yaw;
-
-        const PITCH_LIMIT: f32 = FRAC_PI_2 - 0.01;
-        let pitch = (pitch + delta_pitch).clamp(-PITCH_LIMIT, PITCH_LIMIT);
-
-        transform.rotation = Quat::from_euler(EulerRot::YXZ, yaw, pitch, roll);
+        }
     }
 }
